faedfc38d33f810822d2a701aa909597a7bc7bee
Matthew Ewan-Laws <matt@builtbycactus.co.uk>
2022-02-25 09:37:28 +0000

    fix header z-index

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,4 +1,4 @@
-document.body.onload = () => {
+function initNav() {
   const menuButton = document.querySelector('.js-menu-button')
 
   menuButton.addEventListener('click', function(e) {
@@ -15,4 +15,34 @@ document.body.onload = () => {
         btn.dataset.isOpen = "1"
     }
   })
+}
+
+function gotoSlide(slider, slideNumber) {
+  // getComputedStyle(slider)
+    // .setProperty('--slide', slideNumber.toString())
+  const style = getComputedStyle(slider)
+  const currentSlide = style.getPropertyValue('--slide')
+  slider.style.setProperty('--slide', slideNumber)
+  const newSlide = style.getPropertyValue('--slide')
+  console.log({ slideNumber, newSlide })
+
+}
+
+function initSliders() {
+  const sliders = document.querySelectorAll('.slider')
+
+  sliders.forEach(slider => {
+    const slides = slider.querySelectorAll('.slider__slide')
+
+    slider.querySelectorAll('.slider__pip')
+          .forEach(pip => {
+            const pipSlide = pip.dataset.slide
+            pip.addEventListener('click', () => gotoSlide(slider, pipSlide))
+          })
+  })
+}
+
+document.body.onload = () => {
+  initNav()
+  initSliders()
 }
